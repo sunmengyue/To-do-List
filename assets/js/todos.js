0866389451841ba61ef110bad9e6d71f3bb94b6a
Simon Sun <bardrabbit@gmail.com>
2018-06-19 11:16:34 -0400

    Create a new class and use toggleClass function to toggle the strike through effect

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -6,16 +6,6 @@
 // })
 
 $("li").click(function(){
-	if($(this).css("color") === "rgb(128, 128, 128)") {
-		$(this).css({
-			color: "black",
-			textDecoration: "none"
-		})
-	} else {
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through"
-		})
-	}
+	$(this).toggleClass("completed");
 })
 	
