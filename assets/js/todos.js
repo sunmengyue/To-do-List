9248f5a257846a3a19fb0aa8bdf99c5c37641835
Simon Sun <bardrabbit@gmail.com>
2018-06-19 16:51:18 -0400

    Create new todos with .on()

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,26 @@
 //Check off Specific Todos by Clicking
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
-})
+})//we change it to ul because we can only add listeners to existing elements
+
 
 //Click X to Delete Todos
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove(); //inside the fadeOut, this refferes to the li, not the span
 	});
 	event.stopPropagation(); //stop event from bubbling up
 })
-	
+
+//Create New Todos
+$("input[type = 'text']").on("keypress", function(event){
+	if (event.which === 13) {
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul;
+		console.log($(this).val());
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
+})
