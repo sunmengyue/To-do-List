1942ff63da77ebbf24ef35ea55a0a59ec2a0bb58
Simon Sun <bardrabbit@gmail.com>
2018-06-19 11:48:32 -0400

    Add delete effect

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,11 +1,14 @@
 //Check off Specific Todos by Clicking
 
-// $("li").click(function(){
-// 	$(this).css("color", "gray");
-// 	$(this).css("text-decoration", "line-through");
-// })
-
 $("li").click(function(){
 	$(this).toggleClass("completed");
+})
+
+//Click X to Delete Todos
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove(); //inside the fadeOut, this refferes to the li, not the span
+	});
+	event.stopPropagation(); //stop event from bubbling up
 })
 	
